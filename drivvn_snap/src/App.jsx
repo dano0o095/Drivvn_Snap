aed92c4fc008476c9d809fd6422fc2a87ae7be9b
dano0o095 <danokelly95@hotmail.com>
2021-09-14 21:41:29 +0100

    add Bootstrap refs. Plus small tidy up

---
+++ drivvn_snap/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CardArea from './components/CardArea'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SNAP!</h1>
+      <CardArea />
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  )
+}
+
+export default App;
