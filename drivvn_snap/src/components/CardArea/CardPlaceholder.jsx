f36038d79d0c501b5a1de3c0fca92e3da6abe201
dano0o095 <danokelly95@hotmail.com>
2021-09-16 01:09:47 +0100

    test id

--- drivvn_snap/src/components/CardArea/CardPlaceholder.jsx
+++ drivvn_snap/src/components/CardArea/CardPlaceholder.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CardPlaceholder = () => {
   return (
-    <svg className="m-4 m-4" xmlns="http://www.w3.org/2000/svg" width="220" height="300" viewBox="0 0 220 300">
+    <svg data-testid="placeholder-svg" className="m-4 m-4" xmlns="http://www.w3.org/2000/svg" width="220" height="300" viewBox="0 0 220 300">
       <rect fill="#a8d4e0" width="220" height="300" />
     </svg>
   )
